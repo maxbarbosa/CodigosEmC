9b7b0e71424fe17e248150254fafe165be2b6c8a
maxbarbosa <maxandrade909@gmail.com>
2022-02-05 00:01:04 -0300

    Tipos de váriaveis

---
+++ Curso de Programação em C - eXcript/aula2_variaveis.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    char letra;
+    int idade;
+    double preco;
+    float altura;
+    void; 
+
+    unsigned char A; // caractere sem sinal
+    long int B; // número inteiro, com domínio estendido
+    unsigned int C; // número inteiro positivo
+    unsigned long int D; // número inteiro positivo com domínio estendido
+    short int E; // inteiro com domínio reduzido.
+    unsigned short int F; //inteiro positivo com domínio reduzido
+
+    letra = "M";
+    idade = 19;
+    preco = 2579.371;
+    altura = 1.80;
+
+    return 0;
+}
