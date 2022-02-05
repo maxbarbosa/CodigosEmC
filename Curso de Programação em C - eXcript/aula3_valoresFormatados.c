91120c58fda7889c1d074a3853c53947a72afa5e
maxbarbosa <maxandrade909@gmail.com>
2022-02-05 12:34:04 -0300

    Formatação de valores

---
+++ Curso de Programação em C - eXcript/aula3_valoresFormatados.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+    Máscaras para formatação de prints em C:
+
+        %c para char    ==> Único caractere
+        %i ou %d        ==> Inteiros
+        %f ou %.Nf      ==> Float e double, onde N é a quantidade de casas decimais a serem impressas
+        %u              ==> Módulo de um número
+        %p              ==> Endereço de memória
+        %e ou %E        ==> Números com notação científica
+
+    Símbolos:
+
+        \\              ==> Contrabarra
+        %%              ==> Imprime o sinal de porcentagem
+        \n              ==> Quebra de linha
+        \t              ==> Tabulação
+        \b              ==> Retrocesso
+        \f              ==> Salta página de formulário
+        \0              ==> Nulo
+        
+    */
+
+    //Calculando a área do retângulo
+
+    int base = 5;
+    int altura = 7;
+
+    printf("A área do retângulo é de %d cm.\n", base*altura);
+
+    //Parcelando compras no cartão de crédito
+
+    double preco = 359.99;
+    int parcelas = 4;
+
+    printf("O eletrodoméstico de R$ %.2f pode ser dividido em até %d parcelas de R$ %.2f sem juros.", preco, parcelas, preco/parcelas);
+
+    return 0;
+}
