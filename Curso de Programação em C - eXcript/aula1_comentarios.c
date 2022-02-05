f4b62b2d381fc4731b5f8489b9e1fbecf7ba0054
maxbarbosa <maxandrade909@gmail.com>
2022-02-04 23:45:57 -0300

    Comentários no código

---
+++ Curso de Programação em C - eXcript/aula1_comentarios.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("Exemplos de comentários nas linhas abaixo:");
+
+    //Comentário de uma linha
+
+    /*
+    Lista de comentários:
+    1
+    2
+    3
+    */
+
+   return 0;
+}
