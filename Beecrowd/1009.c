aa470e57959aadc63bf637957de0c59bae58eba9
maxbarbosa <maxandrade909@gmail.com>
2022-02-13 15:10:07 -0300

    Solução da questão 1009

--- Beecrowd/1009.c
+++ Beecrowd/1009.c
@@ -2,5 +2,14 @@
 
 int main(){
 
+    char nome[20];
+    double salario_fixo, total_vendas;
+    
+    scanf("%s", &nome);
+    scanf("%lf", &salario_fixo);
+    scanf("%lf", &total_vendas);
+
+    printf("TOTAL = R$ %.2f\n",  salario_fixo+total_vendas*0.15);
+
     return 0;
 }
